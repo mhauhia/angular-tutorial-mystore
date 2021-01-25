531f11de5a0a468eb2192eaead9e298357039aa3
Markus Hauhia <s2000579@edu.bc.fi>
2021-01-25 13:47:43 +0200

    add product-alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartComponent } from './cart/cart.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
