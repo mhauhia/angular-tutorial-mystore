360c2139b1fa5a8b3e080a438d04a425c2b19e58
Markus Hauhia <s2000579@edu.bc.fi>
2021-01-28 14:09:18 +0200

    add cart-component code for my-store

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service'
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-items;
-  constructor(
-    private cartService: CartService
-  ) {
-    this.items = this.cartService.getItems();
+
+  items;
+  checkoutForm;
+
+  constructor( private cartService: CartService,
+  private formBuilder: FormBuilder ) {
+    this.items = cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+
   }
 
   ngOnInit() {
   }
+
+  onSubmit(customerData) {
+    console.warn('Your order has been submmited', customerData);
+
+    this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
 }
